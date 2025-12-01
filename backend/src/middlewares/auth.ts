ee3d81080035bc4cf27814b442da108b4ebcda9a
mcpeblocker <mcpeblocker@gmail.com>
2025-12-01 21:09:58 +0900

    fix: error handling in auth middleware

--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -3,31 +3,36 @@ import type { RequestWithUser } from "../types/RequestWithUser.js";
 import JWTService from "../services/jwt.service.js";
 import type { AuthTokenPayload } from "../types/token.types.js";
 
+
+const AUTH_ERROR = new Error("Unauthorized");
+
 /**
  * Authentication middleware to protect routes.
  * Checks for a valid authorization token in the request headers.
  * If valid, attaches the userId to the request object.
  */
 export default function authMiddleware(req: RequestWithUser, res: Response, next: NextFunction) {
-    const authHeader = req.headers.authorization;
+    try {
+        const authHeader = req.headers.authorization;
+        if (!authHeader) {
+            throw AUTH_ERROR;
+        }
 
-    if (!authHeader) {
+        const token = authHeader.split(" ")[1];
+        if (!token) {
+            throw AUTH_ERROR;
+        }
+
+        const payload = JWTService.verifyToken(token) as AuthTokenPayload;
+        if (!payload || !payload.userId) {
+            throw AUTH_ERROR;
+        }
+
+        // Attach userId to the request object for downstream handlers
+        req.userId = payload.userId;
+
+        next();
+    } catch (error) {
         return res.status(401).send({ error: "Unauthorized" });
     }
-
-    const token = authHeader.split(" ")[1];
-
-    if (!token) {
-        return res.status(401).send({ error: "Unauthorized" });
-    }
-
-    const payload = JWTService.verifyToken(token) as AuthTokenPayload;
-    if (!payload || !payload.userId) {
-        return res.status(401).send({ error: "Unauthorized" });
-    }
-
-    // Attach userId to the request object for downstream handlers
-    req.userId = payload.userId;
-
-    next();
 }
